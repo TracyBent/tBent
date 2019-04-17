a07ad367993f2a0739803412db7264fad25056f2
B5015386@my.shu.ac.uk <B5015386@my.shu.ac.uk>
2019-04-17 22:16:56 +0100

    3 product pages created

--- itservices/reviewApp/urls.py
+++ itservices/reviewApp/urls.py
@@ -12,5 +12,9 @@ path('contact/', views.contact, name='reviewApp-contact'),
 
 path('smartphone/', views.smartphone, name='reviewApp-smartphone'),
 
+path('smartwatch/', views.smartwatch, name='reviewApp-smartwatch'),
+
+path('tablet/', views.tablet, name='reviewApp-tablet'),
+
 
 ]	
--- itservices/reviewApp/views.py
+++ itservices/reviewApp/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render
 
 
-Reviews = [
+smartphoneReviews = [
 
 {
-	'author': 'Tom Jones',	
+	'author': 'Tom Smith',	
 	'rating': '5 Stars',	
 	'review': 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	
 	'date': '17/04/2019'	
 		
 },
+
+]
+
+smartwatchReviews = [
+
+{
+	'author': 'Tracy Bent',	
+	'rating': '3 Stars',	
+	'review': 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	
+	'date': '17/04/2019'	
+		
+},
+
+
+]
+
+
+tabletReviews = [
+
+{
+	'author': 'Salma Shikdar',	
+	'rating': '2 Stars',	
+	'review': 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	
+	'date': '17/04/2019'	
+		
+},
+
+
 
 ]
 
@@ -25,8 +53,22 @@ def contact(request):
 
 def smartphone(request):
 	daily_report= {
-	'reviews': Reviews
+	'smartphoneReviews': smartphoneReviews
 	}
 	return render (request, 'reviewApp/smartphone.html', daily_report)
+
+def smartwatch(request):
+	daily_report= {
+	'smartwatchReviews': smartwatchReviews
+	}
+	return render (request, 'reviewApp/smartwatch.html', daily_report)
+
+def tablet(request):
+	daily_report= {
+	'tabletReviews': tabletReviews
+	}
+	return render (request, 'reviewApp/tablet.html', daily_report)
+
+
 
 # Create your views here.
