a593de7530ac9b2b77001b7a4b99e0c9f774c699
B5015386@my.shu.ac.uk <B5015386@my.shu.ac.uk>
2019-04-17 20:06:13 +0100

    Created the review page, added templates to home, about, contact & review pages and styled the application using Bootstrap

--- itservices/reviewApp/urls.py
+++ itservices/reviewApp/urls.py
@@ -10,4 +10,7 @@ path('about/', views.about, name='reviewApp-about'),
 
 path('contact/', views.contact, name='reviewApp-contact'),
 
+path('smartphone/', views.smartphone, name='reviewApp-smartphone'),
+
+
 ]	
--- itservices/reviewApp/views.py
+++ itservices/reviewApp/views.py
@@ -1,13 +1,32 @@
 from django.shortcuts import render
 
 
+Reviews = [
+
+{
+	'author': 'Tom Jones',	
+	'rating': '5 Stars',	
+	'review': 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',	
+	'date': '17/04/2019'	
+		
+},
+
+]
+
+
 def home(request):
-	return render (request, 'reviewApp/home.html')
+	return render (request, 'reviewApp/home.html', {'title': 'Home'})
 
 def about(request):
-	return render (request, 'reviewApp/about.html')
+	return render (request, 'reviewApp/about.html', {'title': 'About Us'})
 
 def contact(request):
-	return render (request, 'reviewApp/contact.html')
+	return render (request, 'reviewApp/contact.html', {'title': 'Contact Us'})
+
+def smartphone(request):
+	daily_report= {
+	'reviews': Reviews
+	}
+	return render (request, 'reviewApp/smartphone.html', daily_report)
 
 # Create your views here.
