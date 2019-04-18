31c0bc7c602e1cde12b375dea6954de079a81a73
B5015386@my.shu.ac.uk <B5015386@my.shu.ac.uk>
2019-04-18 23:28:12 +0100

    Setting up a profile for a user account

--- itservices/itapps/urls.py
+++ itservices/itapps/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +31,4 @@ urlpatterns = [
 
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
